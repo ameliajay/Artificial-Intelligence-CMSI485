1674e3994377d04528939c0642a76f1d443336cc
Amelia Jay <amelialjay@gmail.com>
2018-12-17 16:06:14 -0800

    added Homework2a

---
+++ Homework2a/maze_knowledge_base.py
@@ -0,0 +1,100 @@
+'''
+maze_knowledge_base.py
+
+Specifies a simple, Conjunctive Normal Form Propositional
+Logic Knowledge Base for use in Grid Maze pathfinding problems
+with side-information.
+'''
+from maze_clause import MazeClause
+import unittest
+import itertools
+
+class MazeKnowledgeBase:
+
+    def __init__ (self):
+        self.clauses = set()
+
+    def tell (self, clause):
+        """
+        Adds the given clause to the CNF MazeKnowledgeBase
+        Note: we expect that no clause added this way will ever
+        make the KB inconsistent (you need not check for this)
+        """
+        self.clauses.add(clause)
+
+    def ask (self, query):
+        """
+        Given a MazeClause query, returns True if the KB entails
+        the query, False otherwise
+        """
+        clausez = set(self.clauses)
+        new = set()
+
+        # p is a prop and v is the value of the prop (T or F)
+        for p, v in query.props.items():
+            clausez.add(MazeClause([(p, not v)]))
+
+        while True:
+            for each_pair in list(itertools.combinations(clausez, 2)):
+                resolved = MazeClause.resolve(each_pair[0], each_pair[1])
+                for pair in resolved:
+                    if pair.is_empty():
+                        return True
+                    new = new | resolved
+            if new <= clausez:
+                return False
+            clausez = clausez | new
+
+
+class MazeKnowledgeBaseTests(unittest.TestCase):
+    def test_mazekb1(self):
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("X", (1, 1)), True)])))
+
+    def test_mazekb2(self):
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True), (("Y", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Y", (1, 1)), True)])))
+
+    def test_mazekb3(self):
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True)]))
+        kb.tell(MazeClause([(("Y", (1, 1)), False), (("Z", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), True), (("Z", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("W", (1, 1)), True)])))
+        self.assertFalse(kb.ask(MazeClause([(("Y", (1, 1)), False)])))
+
+    def test_mazekb4(self):
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True), (("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), False), (("Z", (1, 1)), False), (("S", (1, 1)), True)]))
+        kb.tell(MazeClause([(("S", (1, 1)), False), (("T", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True), (("T", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("T", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
+
+    def test_mazekb5(self):
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True), (("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), False), (("Z", (1, 1)), False), (("S", (1, 1)), True)]))
+        kb.tell(MazeClause([(("S", (1, 1)), False), (("T", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True), (("T", (1, 1)), True)]))
+        kb.tell(MazeClause([(("W", (1, 1)), True)]))
+        kb.tell(MazeClause([(("T", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), True), (("W", (1, 1)), True)])))
+
+    def test_mazekb6(self):
+        kb = MazeKnowledgeBase()
+        kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), False), (("Z", (1, 1)), False)]))
+        kb.tell(MazeClause([(("X", (1, 1)), True)]))
+        self.assertFalse(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
+        kb.tell(MazeClause([(("Y", (1, 1)), True)]))
+        self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
+
+
+if __name__ == "__main__":
+    unittest.main()
